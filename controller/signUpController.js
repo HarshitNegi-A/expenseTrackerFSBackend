877ea9f50bd3baba1ec2ef86b3508a845d1bbe10
HarshitNegi-A <harshitnegi226@gmail.com>
2025-07-07 13:14:38 +0530

    signupform

--- controller/signUpController.js
+++ controller/signUpController.js
@@ -1,5 +1,21 @@
-exports.signup=(req,res)=>{
-    const {name,email,password}=req.body
-    console.log("Received:", name, email, password)
-    res.status(200).json({ message: "Signup successful!" })
-}
+const db=require('../db')
+
+exports.signup = (req, res) => {
+  const { name, email, password } = req.body;
+
+  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+    if (err) return res.status(500).json({ message: 'DB error' });
+
+    if (results.length > 0) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+    db.query(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password],
+      (err, result) => {
+        if (err) return res.status(500).json({ message: 'Insert error' });
+        res.status(201).json({ message: 'User registered successfully' });
+      }
+    );
+  });
+};
